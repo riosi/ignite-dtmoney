d420f177f434c0fbabb076f1594d2f761dbc24d8
Ingryd Rios <riosingryd@gmail.com>
2021-03-15 16:59:43 -0300

    fixing repository issues

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  return (
+    <div className="App">
+
+<h1>Hello world!</h1>
+
+<GlobalStyle />
+
+   </div>
+  );
+}
+
+
