2ff0b17ee5f88b94f6d0630aacc28f51fb005952
Ingryd Rios <riosingryd@gmail.com>
2021-03-19 23:10:15 -0300

    adding header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { GlobalStyle } from "./styles/global";
+import { Header } from "./components/header/Header";
 
 export function App() {
   return (
-    <div className="App">
+    <>
 
-<h1>Hello world!</h1>
-
+<Header />
 <GlobalStyle />
 
-   </div>
+   </>
   );
 }
 
