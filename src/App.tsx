4e89d8a27a5f125aead74207897e3000a20c7f24
Ingryd Rios <riosingryd@gmail.com>
2021-03-23 15:37:22 -0300

    adding transactions table & fake api

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
-import React from "react";
+
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header/Header";
 import { Dashboard } from "./components/Dashboard";
+import Modal from "react-modal";
+import { useState } from "react";
+
+
+Modal.setAppElement('#root')
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
   return (
     <>
 
-<Header />
+<Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
 <Dashboard />
+
+<Modal 
+  isOpen={isNewTransactionModalOpen} 
+  onRequestClose={handleCloseNewTransactionModal}>
+    <h2>Cadastrar transação</h2>
+</Modal>
+
 <GlobalStyle />
 
    </>
